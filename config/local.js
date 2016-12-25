00c3b9b1f2e328bf44b1a3716b30143db28244f5
do-vuong-wm <vuong.do@west-mec.org>
2016-12-25 12:52:47 -0700

    more fixes to enable faster loading

--- config/local.js
+++ config/local.js
@@ -60,7 +60,7 @@ module.exports = {
    * to 80 (http://) or 443 (https://) if you have an SSL certificate        *
    ***************************************************************************/
 
-   port: process.env.PORT || 80,
+   //port: process.env.PORT || 80,
 
   /***************************************************************************
    * The runtime "environment" of your Sails app is either typically         *
@@ -80,6 +80,7 @@ module.exports = {
    * 'development' environment.                                              *
    ***************************************************************************/
 
-   environment: process.env.NODE_ENV || 'production'
+   environment: process.env.NODE_ENV || 'production',
+   hookTimeout: 90000
 
 };
