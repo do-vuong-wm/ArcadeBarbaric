40e167b5a95ee25ce784c14ff45a9783deff8504
do-vuong-wm <vuong.do@west-mec.org>
2016-12-25 12:29:00 -0700

    Into production mode

--- config/env/production.js
+++ config/env/production.js
@@ -16,16 +16,16 @@ module.exports = {
    * Set the default database connection for models in the production        *
    * environment (see config/connections.js and config/models.js )           *
    ***************************************************************************/
-   //
-   //models: {
-   //  connection: 'mongodb'
-   //},
+
+   models: {
+     connection: 'mongodb'
+   },
 
   /***************************************************************************
    * Set the port in the production environment to 80                        *
    ***************************************************************************/
 
-    //port: 80,
+    port: 80,
 
   /***************************************************************************
    * Set the log level in production environment to "silent"                 *
